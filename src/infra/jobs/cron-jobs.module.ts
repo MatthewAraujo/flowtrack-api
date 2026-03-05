c258204c8f465c5b5ac4ad9f646506d338824e11
Matthew <matthewaraujo.dev@gmail.com>
2026-03-05 12:46:20 -0300

    move profile sync job to cron service

--- src/infra/jobs/cron-jobs.module.ts
+++ src/infra/jobs/cron-jobs.module.ts
@@ -4,12 +4,12 @@ import { CryptographyModule } from '@/infra/cryptography/cryptography.module'
 import { DatabaseModule } from '@/infra/database/database.module'
 import { EnvModule } from '@/infra/env/env.module'
 import { GitHubModule } from '@/infra/github/github.module'
-import { ProfileSyncCronService } from '@/infra/profile/profile-sync.job'
+import { ProfileSyncJob } from '@/infra/jobs/profile-sync.cron.service'
 import { IngestRepositoryActivityUseCase } from '@/domain/flowtrack/application/use-cases/github/ingest-repository-activity'
 import { SyncProfileDataUseCase } from '@/domain/flowtrack/application/use-cases/profile/sync-profile-data'
 
 @Module({
 	imports: [DatabaseModule, CacheModule, CryptographyModule, EnvModule, GitHubModule],
-	providers: [IngestRepositoryActivityUseCase, SyncProfileDataUseCase, ProfileSyncCronService],
+	providers: [IngestRepositoryActivityUseCase, SyncProfileDataUseCase, ProfileSyncJob],
 })
 export class CronJobsModule {}
